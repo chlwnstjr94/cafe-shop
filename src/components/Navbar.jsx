a3dad6021e21dd3b1aac5da9581a09fb0c9c445c
chlwnstjr94 <chlwnstjr940404@gmail.com>
2023-12-26 03:57:22 +0900

    feat: add header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiShoppingBag } from 'react-icons/fi';
+import { BsFillPencilFill } from 'react-icons/bs';
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <header>
+      <Link to='/'>
+        <FiShoppingBag />
+        <h1>CafeShop</h1>
+      </Link>
+      <nav>
+        <Link to='/products'>Products</Link>
+        <Link to='/carts'>Carts</Link>
+        <Link to='/products/new'>
+          <BsFillPencilFill />
+        </Link>
+        <button>Login</button>
+      </nav>
+    </header>
   )
 }
